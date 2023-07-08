b823427607031007b79bd166548ef424dc7a18d2
Tazz Akber <90526357+butteredtoasts@users.noreply.github.com>
2023-07-08 17:22:07 -0400

    Alexandrite blocks, items, and crafting recipes added

---
+++ src/main/java/net/butteredtoasts/mccourse/MCCourseMod.java
@@ -0,0 +1,63 @@
+package net.butteredtoasts.mccourse;
+
+import com.mojang.logging.LogUtils;
+import net.butteredtoasts.mccourse.block.ModBlocks;
+import net.butteredtoasts.mccourse.item.ModCreativeModeTabs;
+import net.butteredtoasts.mccourse.item.ModItems;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.slf4j.Logger;
+
+@Mod(MCCourseMod.MOD_ID)
+public class MCCourseMod {
+    public static final String MOD_ID = "mccourse";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public MCCourseMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModCreativeModeTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
